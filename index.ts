83904e03a511d149c295b2401ef0c452fb1600e2
Julia Khvatova <jkhvatova@gmail.com>
2024-02-27 00:59:58 +0300

    fix: server port

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import { httpServer } from "./src/http_server/index.ts";
-import { wsServer } from "./src/ws_server/index.ts";
+import { httpServer } from "./src/http_server/index";
+import { wsServer } from "./src/ws_server/index";
 
-const HTTP_PORT = 8183;
+const HTTP_PORT = 8181;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT, () => {
